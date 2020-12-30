e57b41a57da6f21017792b21fb0bbf7eaf0c4e3f
AllanMwirigi <allo.mwirigi@gmail.com>
2020-12-30 13:21:42 +0300

    whiteboard controls,styling,tooltips

--- frontend/src/components/Whiteboard.js
+++ frontend/src/components/Whiteboard.js
@@ -1,19 +1,60 @@
 import React,{ useState, useEffect } from "react";
 import { ReactSketchCanvas } from "react-sketch-canvas";
+import ReactTooltip from 'react-tooltip';
 
-const styles = {
-  border: "0.0625rem solid #9c9c9c",
-  borderRadius: "0.25rem",
-};
- 
-export default function Whiteboard() {
-  return (
-    <ReactSketchCanvas
-      style={styles}
-      width="600"
-      height="400"
-      strokeWidth={4}
-      strokeColor="red"
-    />
-  );
-};
+// const styles = {
+//   border: "0.0625rem solid #9c9c9c",
+//   borderRadius: "0.25rem",
+// };
+
+export default class Whiteboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.styles = {
+      border: "0.0625rem solid #9c9c9c",
+      borderRadius: "0.25rem",
+    };
+    this.canvas = React.createRef();
+    this.state = {
+      eraseMode: false
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  whiteBoardUpdated = (data) => {
+    // console.log(data);
+  }
+  toggleEraseMode = () => {
+    this.canvas.current.eraseMode(!this.state.eraseMode);
+    this.setState({ eraseMode: !this.state.eraseMode })
+  }
+
+  render() {
+    return (
+      <div className="whiteboard">
+        <h4>Whiteboard</h4>
+        <ReactTooltip place="top" type="info" effect="float"/>
+        <div className="whiteboard-icons">
+          <i className="fas fa-undo" data-tip='Undo' onClick={() => this.canvas.current.undo()}></i> 
+          <i className="fas fa-redo" data-tip='Redo' onClick={() => this.canvas.current.redo()}></i>
+          <i className="fas fa-eraser" data-tip={this.state.eraseMode ? 'Stop Erase': 'Erase'} onClick={this.toggleEraseMode}></i>
+          <i className="fas fa-broom"data-tip='Clear' onClick={() => this.canvas.current.clearCanvas()}></i>
+        </div>
+        <ReactSketchCanvas
+          ref={this.canvas}
+          style={this.styles}
+          // width="600"
+          // height="400"
+          strokeWidth={4}
+          strokeColor="red"
+          eraserWidth={20}
+          onUpdate={this.whiteBoardUpdated}
+        />
+      </div>
+      
+    );
+  }
+}
--- frontend/src/pages/Workspace.js
+++ frontend/src/pages/Workspace.js
@@ -1,9 +1,13 @@
-import React,{ useState, } from "react";
+import React,{ useState, useEffect } from "react";
 import Whiteboard from '../components/Whiteboard';
+
+// https://www.npmjs.com/package/react-canvas-draw - alternative whiteboard option
+// https://www.npmjs.com/package/react-chat-elements
+// https://www.cometchat.com/tutorials/cometchat-react-chat-elements
 
 export default function Workspace() {
   return(
-    <div>
+    <div className="workspace">
       <Whiteboard/>
     </div>
   );
